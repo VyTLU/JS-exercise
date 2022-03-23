611e1544e52253b7563c1f446eefbf6137e56ac8
Vy Tran Le Uyen <vy.tran@orientsoftware.com>
2022-03-23 11:35:04 +0700

    Add payment page

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-const a=0;
+if (document.readyState == "loading") {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready(){
+    let removeItemButton = document.getElementsByClassName('item-list__remove');
+    for (let i = 0; i < removeItemButton.length; i++) {
+        let button = removeItemButton[i];
+        button.addEventListener('click', removeItem);
+    }
+}
+
+function removeItem(event){
+    let removeButton = event.target;
+    removeButton.parentElement.remove();
+    updateTotal();
+}
+
+function updateTotal(){
+    let itemContainer = document.getElementsByClassName('item-container')[0];
+    let itemLists = itemContainer.getElementsByClassName('item-list');
+    let total = 0;
+    for (let i = 0; i < itemLists.length; i++) {
+        let itemList = itemLists[i];
+        let itemPrice = itemList.getElementsByClassName('item-list__price')[0];
+        let itemQuantity = itemList.getElementsByClassName('item-list__quantity')[0];
+        let price = parseFloat(itemPrice.innerText.replace(''));
+        let quantity = itemQuantity.value;
+        total += (price*quantity);
+    }
+    document.getElementsByClassName('price-total')[0].innerText = total;
+}
