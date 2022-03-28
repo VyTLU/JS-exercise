12c8ce3b2ea632e57c78491824e1f73584303e57
Vy Tran <vy.tran@orientsoftware.com>
2022-03-28 07:39:40 +0700

    Work with localStorage

--- payment.js
+++ payment.js
@@ -1,40 +1,134 @@
-// $(".item-list__photo").click(function(){
-//     $(".item-list__price").load("http://localhost:3001/product");
-// });
-import {product} from './product.js';
+// if (document.readyState == "loading") {
+//     document.addEventListener('DOMContentLoaded', ready);
+// } else {
+//     ready();
+// }
 
-if (document.readyState == "loading") {
-    document.addEventListener('DOMContentLoaded', ready);
-} else {
-    ready();
-}
+// let selectedPrduct = [], paymentProduct = [];
+// function ready(){
+//     let removeItemButton = document.getElementsByClassName('item-list__remove');
+//     for (let i = 0; i < removeItemButton.length; i++) {
+//         let button = removeItemButton[i];
+//         button.addEventListener('click', removeItem);
+//     }
+// }
 
-let selectedPrduct = [], paymentProduct = [];
-function ready(){
-    let removeItemButton = document.getElementsByClassName('item-list__remove');
-    for (let i = 0; i < removeItemButton.length; i++) {
-        let button = removeItemButton[i];
-        button.addEventListener('click', removeItem);
+// function removeItem(event){
+//     let removeButton = event.target;
+//     removeButton.parentElement.remove();
+//     updateTotal();
+// }
+
+// function updateTotal(){
+//     let itemContainer = document.getElementsByClassName('item-container')[0];
+//     let itemLists = itemContainer.getElementsByClassName('item-list');
+//     let total = 0;
+//     for (let i = 0; i < itemLists.length; i++) {
+//         let itemList = itemLists[i];
+//         let itemPrice = itemList.getElementsByClassName('item-list__price')[0];
+//         let itemQuantity = itemList.getElementsByClassName('item-list__quantity')[0];
+//         let price = parseFloat(itemPrice.innerText);
+//         let quantity = itemQuantity.value;
+//         total += (price*quantity);
+//     }
+//     document.getElementsByClassName('price-total')[0].innerText = total;
+// }
+
+const CART = {
+  KEY: "JStest",
+  contents: [],
+  init() {
+    let content = localStorage.getItem(CART.KEY);
+    if (content) {
+      CART.contents = JSON.parse(content);
     }
+
+    CART.sync();
+  },
+  async sync() {
+    let cart = JSON.stringify(CART.contents);
+    await localStorage.setItem(CART.KEY, cart);
+  },
+  reduce(id, qty=1){
+    CART.contents = CART.contents.map(item=>{
+        if(item.id === id)
+            item.quantity = item.quantity - qty;
+        return item;
+    });
+    CART.contents.forEach(async item=>{
+        if(item.id === id && item.quantity === 0)
+            await CART.remove(id);
+    });
+    
+    CART.sync()
+},
+remove(id){
+    CART.contents = CART.contents.filter(item=>{
+        if(item.id !== id)
+            return true;
+    });
+    
+    CART.sync()
+},
+empty(){
+    CART.contents = [];
+    CART.sync()
+},
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  CART.init();
+  showCart();
+});
+
+function showCart() {
+  let itemSection = document.getElementsByClassName("item-container")[0];
+  CART.contents.forEach((item) => {
+    let itemImg = "",
+      itemPrice = "",
+      itemQuantity = "",
+      itemData = "";
+
+    itemImg += `<img class="item-list__photo" src="https://source.unsplash.com/${item.src}" alt="${item.alt}" />`;
+    itemPrice += `<div class="product__wrapper__price">${item.price*item.quantity}</div>`;
+    itemQuantity += `<input class="item-list__quantity" type="number" value="${item.quantity}">`;
+
+    itemData += `<div class="item-list"> 
+                    ${itemImg}
+                    ${itemPrice}
+                    ${itemQuantity}
+                    <button class="item-list__remove">Remove</button>
+                </div>`;
+
+    itemSection.insertAdjacentHTML('beforeend', itemData);
+  });
 }
 
-function removeItem(event){
-    let removeButton = event.target;
-    removeButton.parentElement.remove();
-    updateTotal();
-}
 
-function updateTotal(){
-    let itemContainer = document.getElementsByClassName('item-container')[0];
-    let itemLists = itemContainer.getElementsByClassName('item-list');
-    let total = 0;
-    for (let i = 0; i < itemLists.length; i++) {
-        let itemList = itemLists[i];
-        let itemPrice = itemList.getElementsByClassName('item-list__price')[0];
-        let itemQuantity = itemList.getElementsByClassName('item-list__quantity')[0];
-        let price = parseFloat(itemPrice.innerText);
-        let quantity = itemQuantity.value;
-        total += (price*quantity);
-    }
-    document.getElementsByClassName('price-total')[0].innerText = total;
-}
+// function incrementCart(ev){
+//     ev.preventDefault();
+//     let id = parseInt(ev.target.getAttribute('data-id'));
+//     CART.increase(id, 1);
+//     let controls = ev.target.parentElement;
+//     let qty = controls.querySelector('span:nth-child(2)');
+//     let item = CART.find(id);
+//     if(item){
+//         qty.textContent = item.qty;
+//     }else{
+//         document.getElementById('cart').removeChild(controls.parentElement);
+//     }
+// }
+
+// function decrementCart(ev){
+//     ev.preventDefault();
+//     let id = parseInt(ev.target.getAttribute('data-id'));
+//     CART.reduce(id, 1);
+//     let controls = ev.target.parentElement;
+//     let qty = controls.querySelector('span:nth-child(2)');
+//     let item = CART.find(id);
+//     if(item){
+//         qty.textContent = item.qty;
+//     }else{
+//         document.getElementById('cart').removeChild(controls.parentElement);
+//     }
+// }
--- product.js
+++ product.js
@@ -1,23 +1,90 @@
-let productContainer = document.getElementsByClassName('product')[0];
+let productContainer = document.getElementsByClassName("product")[0];
+let productAdd = document.getElementsByClassName("product__wrapper__add");
+let productData;
 
+const CART = {
+  KEY: "JStest",
+  contents: [],
+  async sync() {
+    let cart = JSON.stringify(CART.contents);
+    await localStorage.setItem(CART.KEY, cart);
+  },
+  find(id) {
+    let match = CART.contents.filter((item) => {
+      return item.id == id;
+    });
+    if (match && match[0]) return match[0];
+  },
+  add(id) {
+    console.log("String " , id);
+    if (CART.find(id)) {
+      CART.increase(id, 1);
+    } else {
+      let obj = {
+        id: productData[id-1].id,
+        src: productData[id-1].src,
+        alt: productData[id-1].alt,
+        price: productData[id-1].price,
+        quantity: 1
+      };
+      CART.contents.push(obj);
+
+      CART.sync();
+    }
+  },
+  increase(id, qty=1){
+    CART.contents = CART.contents.map(item=>{
+        if(item.id === id)
+            item.quantity = item.quantity + qty;
+        return item;
+    });
+    
+    CART.sync()
+},
+};
 
 let product = new XMLHttpRequest();
-product.open('GET', 'http://localhost:3000/product');
-product.onload = function(){
-    let productData = JSON.parse(product.responseText);
-    // console.log(productData[0]);
-    renderHTML(productData);
-}
+product.open("GET", "http://localhost:3001/product");
+product.onload = function () {
+  productData = JSON.parse(product.responseText);
+  
+  renderHTML(productData);
+};
 product.send();
 
-console.log(productContainer);
+function renderHTML(productData) {
+  for (let i = 0; i < productData.length; i++) {
+    const element = productData[i];
+    let productImg = "",
+      productName = "",
+      productPrice = "",
+      data = "";
 
-function renderHTML(productData){
-    // let data = 
-    // for (let i = 0; i < data.length; i++) {
-    //     const element = data[i];
-        
-    // }
+    productImg += `<img class="product__wrapper__img" src="https://source.unsplash.com/${element.src}" alt="${element.alt}" />`;
+    productName += `<div class="product__wrapper__name">${element.alt}</div>`;
+    productPrice += `<div class="product__wrapper__price">${element.price}</div>`;
 
-    productContainer.insertAdjacentHTML('beforeend', "Hello");
+    data += `<div class="product__wrapper">
+              ${productImg}
+              <a class="product__wrapper__add add-cart">Add to cart</a>
+              ${productName}
+              ${productPrice}
+            </div>`;
+
+    productContainer.insertAdjacentHTML("beforeend", data);
+  }
+
+  for (let i = 0; i < productAdd.length; i++) {
+    const element = productAdd[i];
+    element.setAttribute('data-id', productData[i].id);
+    console.log(element);
+    element.addEventListener("click", addProduct);
+  }
+}
+
+function addProduct(element){
+  element.preventDefault();
+  let id = parseInt(element.target.getAttribute('data-id'));
+  console.log(id);
+  CART.add(id, 1);
 }
