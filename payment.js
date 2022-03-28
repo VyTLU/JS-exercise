33458141a996bcab85b5fc2724ccd3f25576cf70
Vy Tran Le Uyen <vy.tran@orientsoftware.com>
2022-03-28 17:14:51 +0700

    Can interact with cart

--- payment.js
+++ payment.js
@@ -34,6 +34,10 @@
 //     document.getElementsByClassName('price-total')[0].innerText = total;
 // }
 
+let productIncrement = document.getElementsByClassName("item-list__increase");
+let productDecrement = document.getElementsByClassName("item-list__decrease");
+let productRemove = document.getElementsByClassName("item-list__remove");
+
 const CART = {
   KEY: "JStest",
   contents: [],
@@ -49,31 +53,46 @@ const CART = {
     let cart = JSON.stringify(CART.contents);
     await localStorage.setItem(CART.KEY, cart);
   },
-  reduce(id, qty=1){
-    CART.contents = CART.contents.map(item=>{
-        if(item.id === id)
-            item.quantity = item.quantity - qty;
-        return item;
+  find(id) {
+    let match = CART.contents.filter((item) => {
+      return item.id == id;
     });
-    CART.contents.forEach(async item=>{
-        if(item.id === id && item.quantity === 0)
-            await CART.remove(id);
+    if (match && match[0]) return match[0];
+  },
+  increase(id, qty = 1) {
+    CART.contents = CART.contents.map(item => {
+      if (item.id === id)
+        item.quantity = item.quantity + qty;
+      return item;
     });
-    
+
     CART.sync()
-},
-remove(id){
-    CART.contents = CART.contents.filter(item=>{
-        if(item.id !== id)
-            return true;
+  },
+  reduce(id, qty = 1) {
+    CART.contents = CART.contents.map(item => {
+      if (item.id === id)
+        item.quantity = item.quantity - qty;
+      return item;
     });
-    
+    CART.contents.forEach(async item => {
+      if (item.id === id && item.quantity === 0)
+        await CART.remove(id);
+    });
+
     CART.sync()
-},
-empty(){
+  },
+  remove(id) {
+    CART.contents = CART.contents.filter(item => {
+      if (item.id !== id)
+        return true;
+    });
+
+    CART.sync()
+  },
+  empty() {
     CART.contents = [];
     CART.sync()
-},
+  },
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -90,45 +109,81 @@ function showCart() {
       itemData = "";
 
     itemImg += `<img class="item-list__photo" src="https://source.unsplash.com/${item.src}" alt="${item.alt}" />`;
-    itemPrice += `<div class="product__wrapper__price">${item.price*item.quantity}</div>`;
-    itemQuantity += `<input class="item-list__quantity" type="number" value="${item.quantity}">`;
+    itemPrice += `<div class="item-list__price">${item.price * item.quantity}</div>`;
+    itemQuantity += `<span id="quantity_${item.id}" class="item-list__quantity">${item.quantity}</span>`;
 
     itemData += `<div class="item-list"> 
                     ${itemImg}
                     ${itemPrice}
-                    ${itemQuantity}
+                    <div class="">
+                      <span class="item-list__increase">+</span>
+                      ${itemQuantity}
+                      <span class="item-list__decrease">-</span>
+                    </div>
                     <button class="item-list__remove">Remove</button>
                 </div>`;
 
     itemSection.insertAdjacentHTML('beforeend', itemData);
   });
+
+  for (let i = 0; i < productIncrement.length; i++) {
+    const element = productIncrement[i];
+    element.setAttribute('data-id', CART.contents[i].id);
+    element.addEventListener('click', incrementCart);
+  }
+
+  for (let i = 0; i < productDecrement.length; i++) {
+    const element = productDecrement[i];
+    element.setAttribute('data-id', CART.contents[i].id);
+    element.addEventListener('click', decrementCart);
+  }
+
+  for (let i = 0; i < productRemove.length; i++) {
+    const element = productRemove[i];
+    element.setAttribute('data-id', CART.contents[i].id);
+    element.addEventListener('click', removeCart);
+  }
 }
 
+function incrementCart(element) {
+  element.preventDefault();
+  let id = parseInt(element.target.getAttribute('data-id'));
+  CART.increase(id, 1);
+  let controls = element.target.parentElement;
+  let qty = controls.querySelector('span:nth-child(2)');
+  let item = CART.find(id);
+  qty.textContent = item.quantity;
+  updateTotal(id, element);
+}
 
-// function incrementCart(ev){
-//     ev.preventDefault();
-//     let id = parseInt(ev.target.getAttribute('data-id'));
-//     CART.increase(id, 1);
-//     let controls = ev.target.parentElement;
-//     let qty = controls.querySelector('span:nth-child(2)');
-//     let item = CART.find(id);
-//     if(item){
-//         qty.textContent = item.qty;
-//     }else{
-//         document.getElementById('cart').removeChild(controls.parentElement);
-//     }
-// }
+function decrementCart(element) {
+  element.preventDefault();
+  let id = parseInt(element.target.getAttribute('data-id'));
+  CART.reduce(id, 1);
+  let controls = element.target.parentElement;
+  let qty = controls.querySelector('span:nth-child(2)');
+  let item = CART.find(id);
+  if (item) {
+    qty.textContent = item.quantity;
+  } else {
+    controls.parentElement.remove();
+  }
+  updateTotal(id, element);
+}
 
-// function decrementCart(ev){
-//     ev.preventDefault();
-//     let id = parseInt(ev.target.getAttribute('data-id'));
-//     CART.reduce(id, 1);
-//     let controls = ev.target.parentElement;
-//     let qty = controls.querySelector('span:nth-child(2)');
-//     let item = CART.find(id);
-//     if(item){
-//         qty.textContent = item.qty;
-//     }else{
-//         document.getElementById('cart').removeChild(controls.parentElement);
-//     }
-// }
+function removeCart(element) {
+  element.preventDefault();
+  let id = parseInt(element.target.getAttribute('data-id'));
+  element.target.parentElement.remove();
+  CART.remove(id);
+}
+
+function updateTotal(id, element) {
+  let item = CART.find(id);
+  if (item) {
+    let total = item.price * item.quantity;
+
+    let prc = element.target.parentElement.parentElement.querySelector(".item-list__price");
+    prc.textContent = total;
+  }
+}
