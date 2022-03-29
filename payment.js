996c28fd6fa6e906368e9d9a16fd94a9c633c4c5
Vy Tran <vy.tran@orientsoftware.com>
2022-03-29 07:43:27 +0700

    Template

--- payment.js
+++ payment.js
@@ -1,38 +1,5 @@
-// if (document.readyState == "loading") {
-//     document.addEventListener('DOMContentLoaded', ready);
-// } else {
-//     ready();
-// }
-
-// let selectedPrduct = [], paymentProduct = [];
-// function ready(){
-//     let removeItemButton = document.getElementsByClassName('item-list__remove');
-//     for (let i = 0; i < removeItemButton.length; i++) {
-//         let button = removeItemButton[i];
-//         button.addEventListener('click', removeItem);
-//     }
-// }
-
-// function removeItem(event){
-//     let removeButton = event.target;
-//     removeButton.parentElement.remove();
-//     updateTotal();
-// }
-
-// function updateTotal(){
-//     let itemContainer = document.getElementsByClassName('item-container')[0];
-//     let itemLists = itemContainer.getElementsByClassName('item-list');
-//     let total = 0;
-//     for (let i = 0; i < itemLists.length; i++) {
-//         let itemList = itemLists[i];
-//         let itemPrice = itemList.getElementsByClassName('item-list__price')[0];
-//         let itemQuantity = itemList.getElementsByClassName('item-list__quantity')[0];
-//         let price = parseFloat(itemPrice.innerText);
-//         let quantity = itemQuantity.value;
-//         total += (price*quantity);
-//     }
-//     document.getElementsByClassName('price-total')[0].innerText = total;
-// }
+//https://www.figma.com/community/file/888726891176991741
+//https://www.behance.net/gallery/80515301/Wavey-beauty-UI
 
 let productIncrement = document.getElementsByClassName("item-list__increase");
 let productDecrement = document.getElementsByClassName("item-list__decrease");
